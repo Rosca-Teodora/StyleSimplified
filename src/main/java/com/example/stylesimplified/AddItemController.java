aebd4c9a305d6aaa72a9ccb72f4f730d6bdf5988
r0scat <maia.rosca@gmail.com>
2026-04-29 11:59:04 +0300

    start implementation of image selection and preview

--- src/main/java/com/example/stylesimplified/AddItemController.java
+++ src/main/java/com/example/stylesimplified/AddItemController.java
@@ -9,23 +9,33 @@ import com.example.stylesimplified.backend.models.ClothingItem;
 import com.example.stylesimplified.backend.models.Top;
 import com.example.stylesimplified.backend.services.WardrobeService;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.scene.image.Image;
 
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+import java.io.File;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class AddItemController implements Initializable {
+    // general clothing item fields
     @FXML
     private TextField nameField;
     @FXML
-    private TextField imagePathField;
+    private ImageView imagePreview;
+    private File selectedImageFile = null; // to remember selected file (for clothing item images)
     @FXML
     private ChoiceBox<String> typeChoiceBox;
     @FXML
@@ -85,17 +95,73 @@ public class AddItemController implements Initializable {
         accessoryInputsContainer.setManaged("Accessory".equals(selectedType));
     }
 
+    // show selected image for clothes
+    @FXML
+    void showClothingDetails(ClothingItem clothingItem){
+
+    }
+
+    @FXML
+    void handleSelectImageButton() {
+        // despre fileChooser: https://docs.oracle.com/javase/8/javafx/api/javafx/stage/FileChooser.html
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Current clothing image: ");
+
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpeg", "*.gif")
+        );
+
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                // get selected file -> open window to select image
+                File file = fileChooser.showOpenDialog(null); // null instead of current stage
+
+                if (file != null) {
+                    Image preview = new Image(file.toURI().toString(), 100, 0, false, false);
+                    imagePreview.setImage(preview);
+                }
+            }
+        }
+    }
+
     @FXML
     void handleAddItemButton(ActionEvent event) {
         String name = nameField.getText();
-        String imagePath = imagePathField.getText();
+        String imagePath = "images/default.png"; // in cazul in care nu s-a adaugat nicio imagine exista un default
         String type = typeChoiceBox.getValue();
 
+        // verificare field-uri ClotjingItem (cele impartite si de restul subclaselor)
         if (name.isEmpty()) {
             statusLabel.setText("Item must have a name!");
             statusLabel.setTextFill(Color.RED);
             return;
         }
+
+        if (selectedImageFile != null) {
+            try {
+                // 1. Define where you want to copy the image to
+                String fileName = System.currentTimeMillis() + "_" + selectedImageFile.getName(); // Make name unique
+                Path destinationDir = Paths.get("wardrobe_images");
+
+                // Create the folder if it doesn't exist
+                if (!Files.exists(destinationDir)) {
+                    Files.createDirectories(destinationDir);
+                }
+
+                Path destinationPath = destinationDir.resolve(fileName);
+
+                // 2. Actually copy the file on the hard drive
+                Files.copy(selectedImageFile.toPath(), destinationPath, StandardCopyOption.REPLACE_EXISTING);
+
+                // 3. Save the relative text path
+                finalImagePath = destinationPath.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
 
         ClothingItem newClothingItem = null;
         try {
@@ -135,7 +201,7 @@ public class AddItemController implements Initializable {
 
     private void clearFields() {
         nameField.clear();
-        imagePathField.clear();
+        //imagePathField.clear();
         sleeveInput.clear();
         necklineInput.clear();
         isOuterwearInput.setSelected(false);
