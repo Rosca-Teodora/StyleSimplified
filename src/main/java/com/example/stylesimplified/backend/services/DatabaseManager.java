6b8cabc1f9616742790d6d3fc5abbe1a1558b618
r0scat <maia.rosca@gmail.com>
2026-05-12 13:53:08 +0300

    change to pooled connection to database

--- src/main/java/com/example/stylesimplified/backend/services/DatabaseManager.java
+++ src/main/java/com/example/stylesimplified/backend/services/DatabaseManager.java
@@ -1,28 +1,46 @@
 package com.example.stylesimplified.backend.services;
-
-import com.j256.ormlite.jdbc.JdbcConnectionSource;
-import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 import com.example.stylesimplified.backend.models.*;
-
-import java.sql.SQLException;
 
 public class DatabaseManager {
     // baza de date intr-un container docker pe portul 5432 cu environment-ul setat asa pt development
     public static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/wardrobe_db?user=admin&password=password123";
+    // singleton pattern pt database pool
+    private static JdbcPooledConnectionSource connectionSource;
+    private DatabaseManager(){}
+
+    public static JdbcPooledConnectionSource getDatabase() throws Exception {
+        if (connectionSource == null) {
+            connectionSource = new JdbcPooledConnectionSource(DATABASE_URL);
+            connectionSource.setTestBeforeGet(true); // verifica sa nu fi murit conexiunea cu baza de date inainte sa fie trimisa
+        }
+        return connectionSource;
+    }
 
     public static void setupDatabase() {
-        try (JdbcConnectionSource connectionSource = new JdbcConnectionSource(DATABASE_URL)) {
-            // table creation
-            TableUtils.createTable(connectionSource, Top.class);
-            TableUtils.createTable(connectionSource, Bottom.class);
-            TableUtils.createTable(connectionSource, Accessory.class);
-            System.out.println("PostgreSQL docker container started and tables created");
-        } catch (SQLException e) {
-            System.out.println("SQL exception occured");
-            e.printStackTrace();
+        try {
+            JdbcPooledConnectionSource sursa = getDatabase();
+
+            TableUtils.createTable(sursa, Top.class);
+            TableUtils.createTable(sursa, Bottom.class);
+            TableUtils.createTable(sursa, Accessory.class);
+            TableUtils.createTable(sursa, Tag.class);
+
+            System.out.println("PostgreSQL with docker connected + tables made");
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    // e nevoie de metode closeConnection fiindca altfel pot aparea memory leak-uri
+    public static void closeConnection() {
+        if (connectionSource != null){
+            try {
+                connectionSource.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
